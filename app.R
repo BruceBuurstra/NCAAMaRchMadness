c5c7e9a01fdfdf5e8e9fa856e53bfadc6b59c73b
BruceBuurstra <buursbru@mail.gvsu.edu>
2021-10-12 13:28:37 +0000

    Starting project

---
+++ app.R
@@ -0,0 +1,19 @@
+library(tidyverse)
+library(readr)
+Big_Dance_CSV <- read_csv("Big_Dance_CSV.csv")
+shinyApp(
+  ui = fluidPage(
+    selectInput("seed1", "Team Seed:",
+                c(1:16)),
+    selectInput("seed2", "Opponent Seed:",
+                c(1:16)),
+    print("hello"),
+    tableOutput("data"), 
+  ),
+  server = function(input, output) {
+    output$data <- renderTable({
+      Big_Dance_CSV %>%
+        filter(`Seed` %in% input$seed1 & `Seed_1` %in% input$seed2 | `Seed_1` %in% input$seed1 & `Seed` %in% input$seed2)
+    }, rownames = TRUE)
+  }
+)
