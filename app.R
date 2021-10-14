8f86f3fa247e1045d4b9b46b1a77b39181756cfd
Connor <mcnameco@mail.gvsu.edu>
2021-10-14 13:12:38 +0000

    added year slider

--- app.R
+++ app.R
@@ -7,13 +7,18 @@ shinyApp(
                 c(1:16)),
     selectInput("seed2", "Opponent Seed:",
                 c(1:16)),
+    sliderInput(inputId = "start year",
+                label = "Number of bins:",
+                min = 1985,
+                max = 2021,
+                value = 1985),
     print("hello world"),
     tableOutput("data"), 
   ),
   server = function(input, output) {
     output$data <- renderTable({
       Big_Dance_CSV %>%
-        filter(`Seed` %in% input$seed1 & `Seed_1` %in% input$seed2 | `Seed_1` %in% input$seed1 & `Seed` %in% input$seed2)
+        filter(`Seed` %in% input$seed1 & `Seed_1` %in% input$seed2 | `Seed_1` %in% input$seed1 & `Seed` %in% input$seed2, Year >= input$`start year`) 
     }, rownames = TRUE)
   }
 )
