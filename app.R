cf7318b827a4b3a4a419abcac1b4da16a2614c5a
BruceBuurstra <buursbru@mail.gvsu.edu>
2021-10-25 22:24:47 +0000

    pulling in files

--- app.R
+++ app.R
@@ -1,8 +1,10 @@
 library(tidyverse)
+library(shinythemes)
 library(readr)
 Big_Dance_CSV <- read_csv("Big_Dance_CSV.csv")
 shinyApp(
   ui = fluidPage(
+    navbarPage("NCAA Swimming", theme = shinytheme("lumen")),
     selectInput("seed1", "Team Seed:",
                 c(1:16)),
     selectInput("seed2", "Opponent Seed:",
