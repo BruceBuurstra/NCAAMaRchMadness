c890005f33ef4c533baf731d223cc1c6ce879209
BruceBuurstra <buursbru@mail.gvsu.edu>
2021-10-28 13:18:14 +0000

    .

--- app.R
+++ app.R
@@ -4,30 +4,6 @@ library(shiny)
 library(gt)
 library(gtExtras)
 Big_Dance_CSV <- read_csv("Big_Dance_CSV.csv")
-Big_Dance_Seeds <- Big_Dance_CSV %>% 
-  mutate("high seed" = case_when(Seed < Seed_1 ~ Seed,
-                                 Seed_1 < Seed ~ Seed_1,
-                                 Seed == Seed_1 ~ Seed),
-         "low seed" = case_when(Seed > Seed_1 ~ Seed,
-                                Seed_1 > Seed ~ Seed_1,
-                                Seed == Seed_1 ~ Seed),
-         "high seed team" = case_when(Seed < Seed_1 ~ Team,
-                                      Seed_1 < Seed ~ Team_1,
-                                      Seed == Seed_1 ~ Team),
-         "low seed team" = case_when(Seed > Seed_1 ~ Team,
-                                     Seed_1 > Seed ~ Team_1,
-                                     Seed == Seed_1 ~ Team_1),
-         "high score" = case_when(Seed < Seed_1 ~ Score,
-                                  Seed_1 < Seed ~ Score_1),
-         "low score" = case_when(Seed > Seed_1 ~ Score,
-                                 Seed_1 > Seed ~ Score_1),
-         "winning score" = case_when(Score > Score_1 ~ Score,
-                                     Score_1 > Score ~ Score_1),
-         "losing score" = case_when(Score < Score_1 ~ Score,
-                                    Score_1 < Score ~ Score_1),
-         "high seed win" = case_when(`high score` > `low score` ~ 1,
-                                     `high score` < `low score` ~ 0)) %>% 
-  select(-Team, -Team_1, -Score, -Score_1, -Seed, -Seed_1)
 shinyApp(
   ui = fluidPage(
     selectInput("seed1", "Team Seed:",
